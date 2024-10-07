0b389bb417356d7152bd917568dccc10f498c452
ziphiro <xgrachx@gmail.com>
2024-10-07 21:51:28 +0300

    добавлен файл для логирования;

--- src/main/java/com/ziphiro/browserBotPart/service/UserFileService.java
+++ src/main/java/com/ziphiro/browserBotPart/service/UserFileService.java
@@ -4,6 +4,9 @@ import com.ziphiro.browserBotPart.entityes.UserFile;
 import com.ziphiro.browserBotPart.repositories.UserFileRepository;
 import com.ziphiro.browserBotPart.values.StrV;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -20,31 +23,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserFileService {
 
     private final UserFileRepository userFileRepository;
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserFileService.class);
     public List<UserFile> getAllUserFilesPaths(String name){
         return userFileRepository.findFilesByCreator(name);
     }
 
     @Async("asyncDownload")
     public CompletableFuture<ResponseEntity<Resource>> downloadFile(String fileName, String userName) throws IOException {
+        LOGGER.info(userName + StrV.STARTING_DOWNLOAD + fileName);
         Path filePath  = Path.of(StrV.STORAGE_DIR, userName, fileName);
         InputStream fileStream = new FileInputStream(filePath.toFile());
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" +
                 fileName + StrV.SLASH);
+        LOGGER.info(userName + StrV.COMPLETE_DOWNLOAD + fileName);
         return CompletableFuture.completedFuture(ResponseEntity.ok().headers(headers).contentLength(filePath.toFile().length())
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(new InputStreamResource(fileStream)));
     }
     @Async("asyncUpload")
     public CompletableFuture<String> uploadFile(MultipartFile file, String userName) throws IOException {
+        LOGGER.info(userName + StrV.UPLOAD_START + file.getOriginalFilename());
         if (file.isEmpty()){
             throw new IOException(StrV.UPLOAD_ERROR);
         } else {
@@ -55,6 +61,7 @@ public class UserFileService {
                     .creator(userName).build();
             userFileRepository.save(uploadFile);
         }
+        LOGGER.info(userName + StrV.UPLOAD_SUCCESS + file.getOriginalFilename());
         return CompletableFuture.completedFuture(StrV.UPLOAD_SUCCESS);
     }
 
--- src/main/java/com/ziphiro/browserBotPart/values/StrV.java
+++ src/main/java/com/ziphiro/browserBotPart/values/StrV.java
@@ -4,6 +4,7 @@ public class StrV {
 
     public static final String SLASH = "/";
     public static final String UPLOAD_ERROR = "upload error";
+    public static final String UPLOAD_START = "upload start";
     public static final String UPLOAD_SUCCESS = "upload success";
     public static final String STORAGE_DIR = "/home/ziphiro/myBotStorage/";
     public static final String LOGIN_FAILED = "";
@@ -11,4 +12,6 @@ public class StrV {
     public static final String REGISTERED = " registered";
     public static final String FAILED_REGISTRATION = " failed registration";
     public static final String PLUS = "+";
+    public static final String STARTING_DOWNLOAD = " downloading file: ";
+    public static final String COMPLETE_DOWNLOAD = " downloaded file: ";
 }
